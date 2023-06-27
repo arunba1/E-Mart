73e4016274ff241dde3cb14a76a7fc476b788139
arunbalaji123 <arunbalaji211@gmail.com>
2023-06-27 22:42:06 +0530

    Initial Commit

--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -1,12 +1,58 @@
 import 'package:e_mart/consts.dart';
+import 'package:e_mart/widget_common/bg_widget.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-    );
+    return bgWidget(
+        child: Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            //edit profile button
+            const Align(
+              alignment: Alignment.topRight,
+              child: Icon(Icons.edit,color: whiteColor,)).onTap((){}),
+
+
+            //users detail section
+            Row(
+              children: [
+                Image.asset(
+                  imgProfile2,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ).box.roundedFull.clip(Clip.antiAlias).make(),
+                10.widthBox,
+                Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    "Dummy User".text.fontFamily(semibold).white.make(),
+                    "customer@gmail.com".text.white.make(),
+                  ],
+                )),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      color: whiteColor,
+                    )
+                  ),
+                  onPressed: (){}, child: logout.text.fontFamily(semibold).white.make()
+                  ),
+              ],
+            ),
+            20.heightBox,
+            Column().box.white.rounded.make(),
+          ],
+
+
+        ),
+      )),
+    ));
   }
 }
